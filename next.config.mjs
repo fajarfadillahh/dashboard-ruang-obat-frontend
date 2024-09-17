4e0bba8f32ff7652c1fccc51cc31b798f8504871
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-09-17 22:02:33 +0700

    setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    optimizePackageImports: ["@nextui-org/react", "@phosphor-icons/react"],
+  },
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,49 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        lg: "1200px",
+      },
+    },
     extend: {
+      fontFamily: {
+        sans: ["Mulish", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#171717",
+        gray: {
+          DEFAULT: "#525252",
+        },
+        purple: {
+          DEFAULT: "#6238C3",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            secondary: {
+              DEFAULT: "#6238C3", // purple
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
