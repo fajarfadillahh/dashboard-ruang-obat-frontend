ef5333e9680ca84bcd689ceb127bdba674dbb06c
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-10-13 23:08:14 +0700

    created formatName function for fullname on dropdown navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,6 +12,21 @@ import { signOut, useSession } from "next-auth/react";
 export default function Navbar() {
   const session = useSession();
 
+  const formatName = (name: string): string => {
+    const parts: string[] = name.split(" ");
+    let result: string;
+
+    if (parts.length === 1) {
+      result = parts[0];
+    } else if (parts.length === 2) {
+      result = `${parts[0]} ${parts[1].charAt(0)}.`;
+    } else {
+      result = `${parts[0]} ${parts[1].charAt(0)}. ${parts[2].charAt(0)}.`;
+    }
+
+    return result;
+  };
+
   return (
     <nav className="bg-white px-6">
       <div className="flex h-20 items-center justify-end">
@@ -32,7 +47,7 @@ export default function Navbar() {
               <div>
                 <h6 className="text-sm font-bold text-black">
                   {session.status == "authenticated"
-                    ? session.data.user.fullname
+                    ? formatName(session.data.user.fullname)
                     : ""}
                 </h6>
                 <p className="text-[12px] font-semibold uppercase text-gray">
