7b2a7100e3304781acabfc90c5be0c1635b4a878
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-11-23 22:24:36 +0700

    deleted _dummy folder

--- _dummy/users.ts
+++
@@ -1,9 +0,0 @@
-import { UserType } from "@/types/user.type";
-
-export const users: UserType[] = [
-  {
-    user_id: "ROUAP123456",
-    fullname: "Arya Pratama",
-    university: "Cambridge",
-  },
-];
