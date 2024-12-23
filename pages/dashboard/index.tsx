93d7551c3a87d34405d2fd8ac0bf89155a656d57
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-12-23 15:27:59 +0700

    modified dashboard page to use IconContext and DashbaordButton component

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -11,6 +11,7 @@ import {
   ArrowRight,
   Certificate,
   ClipboardText,
+  IconContext,
   User,
   UserCheck,
 } from "@phosphor-icons/react";
@@ -22,7 +23,6 @@ import useSWR from "swr";
 export default function DashboardPage({
   token,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const router = useRouter();
   const { data, error, isLoading } = useSWR<SuccessResponse<DashboardResponse>>(
     {
       url: "/admin/dashboard",
@@ -85,131 +85,107 @@ export default function DashboardPage({
           <div className="grid gap-6 rounded-xl border-[2px] border-gray/20 px-16 py-8">
             <h4 className="font-bold text-black">Ringkasan Aplikasi CBT</h4>
 
-            <div className="grid grid-cols-2 divide-x-2 divide-gray/20">
-              <div className="grid divide-y-2 divide-gray/20 pr-12">
-                <div className="grid pb-8">
-                  <div className="inline-flex items-center gap-4">
-                    <User weight="duotone" size={64} className="text-black" />
+            <IconContext.Provider
+              value={{
+                weight: "bold",
+                size: 64,
+                className: "text-purple",
+              }}
+            >
+              <div className="grid grid-cols-2 divide-x-2 divide-gray/20">
+                <div className="grid divide-y-2 divide-gray/20 pr-12">
+                  <div className="grid pb-8">
+                    <div className="inline-flex items-center gap-4">
+                      <User />
 
-                    <div>
-                      <p className="text-sm font-medium text-gray">
-                        Total Pengguna
-                      </p>
-                      <h6 className="text-[32px] font-extrabold text-black">
-                        {data?.data.total_users}
-                      </h6>
+                      <div>
+                        <p className="text-sm font-medium text-gray">
+                          Total Pengguna
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          {data?.data.total_users}
+                        </h6>
+                      </div>
                     </div>
+
+                    <DashbaordButton path="/users" />
                   </div>
 
-                  <Button
-                    variant="light"
-                    color="secondary"
-                    size="sm"
-                    endContent={<ArrowRight weight="bold" size={16} />}
-                    onClick={() => router.push("/users")}
-                    className="mt-2 w-max px-4 font-bold"
-                  >
-                    Lihat Detail
-                  </Button>
+                  <div className="grid pt-8">
+                    <div className="inline-flex items-center gap-4">
+                      <Certificate />
+
+                      <div>
+                        <p className="text-sm font-medium text-gray">
+                          Total Program
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          {data?.data.total_programs}
+                        </h6>
+                      </div>
+                    </div>
+
+                    <DashbaordButton path="/programs" />
+                  </div>
                 </div>
 
-                <div className="grid pt-8">
-                  <div className="inline-flex items-center gap-4">
-                    <Certificate
-                      weight="duotone"
-                      size={64}
-                      className="text-black"
-                    />
+                <div className="grid divide-y-2 divide-gray/20 pl-12">
+                  <div className="grid pb-8">
+                    <div className="inline-flex items-center gap-4">
+                      <UserCheck />
 
-                    <div>
-                      <p className="text-sm font-medium text-gray">
-                        Total Program
-                      </p>
-                      <h6 className="text-[32px] font-extrabold text-black">
-                        {data?.data.total_programs}
-                      </h6>
+                      <div>
+                        <p className="text-sm font-medium text-gray">
+                          Pengguna Aktif
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          {data?.data.total_online_users}
+                        </h6>
+                      </div>
                     </div>
+
+                    <DashbaordButton path="/sessions" />
                   </div>
 
-                  <Button
-                    variant="light"
-                    color="secondary"
-                    size="sm"
-                    endContent={<ArrowRight weight="bold" size={16} />}
-                    onClick={() => router.push("/programs")}
-                    className="mt-2 w-max px-4 font-bold"
-                  >
-                    Lihat Detail
-                  </Button>
-                </div>
-              </div>
+                  <div className="grid pt-8">
+                    <div className="inline-flex items-center gap-4">
+                      <ClipboardText />
 
-              <div className="grid divide-y-2 divide-gray/20 pl-12">
-                <div className="grid pb-8">
-                  <div className="inline-flex items-center gap-4">
-                    <UserCheck
-                      weight="duotone"
-                      size={64}
-                      className="text-black"
-                    />
-
-                    <div>
-                      <p className="text-sm font-medium text-gray">
-                        Pengguna Aktif
-                      </p>
-                      <h6 className="text-[32px] font-extrabold text-black">
-                        {data?.data.total_online_users}
-                      </h6>
+                      <div>
+                        <p className="text-sm font-medium text-gray">
+                          Total Ujian
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          {data?.data.total_tests}
+                        </h6>
+                      </div>
                     </div>
+
+                    <DashbaordButton path="/tests" />
                   </div>
-
-                  <Button
-                    variant="light"
-                    color="secondary"
-                    size="sm"
-                    endContent={<ArrowRight weight="bold" size={16} />}
-                    onClick={() => router.push("/sessions")}
-                    className="mt-2 w-max px-4 font-bold"
-                  >
-                    Lihat Detail
-                  </Button>
-                </div>
-
-                <div className="grid pt-8">
-                  <div className="inline-flex items-center gap-4">
-                    <ClipboardText
-                      weight="duotone"
-                      size={64}
-                      className="text-black"
-                    />
-
-                    <div>
-                      <p className="text-sm font-medium text-gray">
-                        Total Ujian
-                      </p>
-                      <h6 className="text-[32px] font-extrabold text-black">
-                        {data?.data.total_tests}
-                      </h6>
-                    </div>
-                  </div>
-
-                  <Button
-                    variant="light"
-                    color="secondary"
-                    size="sm"
-                    endContent={<ArrowRight weight="bold" size={16} />}
-                    onClick={() => router.push("/tests")}
-                    className="mt-2 w-max px-4 font-bold"
-                  >
-                    Lihat Detail
-                  </Button>
                 </div>
               </div>
-            </div>
+            </IconContext.Provider>
           </div>
         </section>
       </Container>
     </Layout>
+  );
+}
+
+function DashbaordButton({ path }: { path: string }) {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant="light"
+      size="sm"
+      endContent={<ArrowRight weight="bold" size={14} className="text-black" />}
+      onClick={() => router.push(path)}
+      className="mt-2 w-max font-bold text-black"
+    >
+      Lihat Detail
+    </Button>
   );
 }
 
