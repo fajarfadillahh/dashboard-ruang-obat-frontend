7b0c7d673322b630ff9f3f3817d9a9107b06724e
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-01-22 20:34:30 +0700

    implemented detail subject private & modified text footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer className="flex h-16 items-center justify-center px-6 text-center">
-      <p className="text-gray text-sm font-medium capitalize">
-        &copy; PT. Pharmacy Cone Group {new Date().getFullYear()}
+      <p className="text-sm font-medium capitalize text-gray">
+        &copy; Part of{" "}
+        <span className="font-bold text-purple">Pharma Metrocity Group</span>{" "}
+        {new Date().getFullYear()}
       </p>
     </footer>
   );
