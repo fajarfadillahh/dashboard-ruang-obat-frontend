a7482d2b1b2be9619712f2407f5a92af0f97e601
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-05-21 22:37:00 +0700

    feat(ai limit users): integrated api for ai limit users page

---
+++ pages/ai/limits-users/index.tsx
@@ -0,0 +1,254 @@
+import ButtonBack from "@/components/button/ButtonBack";
+import CustomTooltip from "@/components/CustomTooltip";
+import EmptyData from "@/components/EmptyData";
+import ErrorPage from "@/components/ErrorPage";
+import LoadingScreen from "@/components/loading/LoadingScreen";
+import ModalConfirm from "@/components/modal/ModalConfirm";
+import SearchInput from "@/components/SearchInput";
+import TitleText from "@/components/TitleText";
+import Container from "@/components/wrapper/Container";
+import Layout from "@/components/wrapper/Layout";
+import useSearch from "@/hooks/useSearch";
+import { withToken } from "@/lib/getToken";
+import { getUrl } from "@/lib/getUrl";
+import { LimitAIUser, LimitAIUserResponse } from "@/types/ai/limit.type";
+import { SuccessResponse } from "@/types/global.type";
+import { customStyleTable } from "@/utils/customStyleTable";
+import { formatDate } from "@/utils/formatDate";
+import {
+  Button,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { PencilLine, Plus, Trash } from "@phosphor-icons/react";
+import { InferGetServerSidePropsType } from "next";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import { useEffect } from "react";
+import useSWR from "swr";
+
+export default function AILimitsCustomPage({
+  token,
+  query,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  const { setSearch, searchValue } = useSearch(800);
+  const { data, isLoading, error } = useSWR<
+    SuccessResponse<LimitAIUserResponse>
+  >({
+    url: getUrl("/ai/limits/users", query),
+    method: "GET",
+    token,
+  });
+
+  useEffect(() => {
+    if (searchValue) {
+      router.push({ query: { q: searchValue } });
+    } else {
+      router.push("/ai/limits-users");
+    }
+  }, [searchValue]);
+
+  const columnsLimitUser = [
+    { name: "ID Pengguna", uid: "user_id" },
+    { name: "Nama Pengguna", uid: "fullname" },
+    { name: "Total", uid: "total" },
+    { name: "Dibuat Pada", uid: "created_at" },
+    { name: "Expired Pada", uid: "expired_at" },
+    { name: "Aksi", uid: "action" },
+  ];
+
+  function renderCellLimitUsers(user: LimitAIUser, columnKey: React.Key) {
+    const cellValue = user[columnKey as keyof LimitAIUser];
+
+    switch (columnKey) {
+      case "user_id":
+        return <div className="font-medium text-black">{user.user_id}</div>;
+      case "fullname":
+        return <div className="font-medium text-black">{user.fullname}</div>;
+      case "total":
+        return <div className="font-medium text-black">{user.total}</div>;
+      case "created_at":
+        return (
+          <div className="font-medium text-black">
+            {formatDate(user.created_at)}
+          </div>
+        );
+      case "expired_at":
+        return (
+          <div className="font-medium text-black">
+            {formatDate(user.expired_at)}
+          </div>
+        );
+      case "action":
+        return (
+          <div className="inline-flex items-center gap-2">
+            <Button isIconOnly variant="light" size="sm" color="secondary">
+              <CustomTooltip content="Edit Pengguna">
+                <PencilLine weight="duotone" size={18} />
+              </CustomTooltip>
+            </Button>
+
+            <ModalConfirm
+              trigger={
+                <Button isIconOnly variant="light" color="danger" size="sm">
+                  <CustomTooltip content="Hapus LPengguna">
+                    <Trash weight="duotone" size={18} className="text-danger" />
+                  </CustomTooltip>
+                </Button>
+              }
+              header={<h1 className="font-bold text-black">Hapus Pengguna</h1>}
+              body={
+                <div className="grid gap-3 text-sm font-medium">
+                  <p className="leading-[170%] text-gray">
+                    Apakah anda ingin menghapus limit{" "}
+                    <strong className="font-extrabold text-purple">
+                      {user.fullname}
+                    </strong>
+                    ?
+                  </p>
+                </div>
+              }
+              footer={(onClose: any) => (
+                <>
+                  <Button
+                    color="danger"
+                    variant="light"
+                    onPress={onClose}
+                    className="font-semibold"
+                  >
+                    Tutup
+                  </Button>
+
+                  <Button color="danger" className="font-semibold">
+                    Ya, Hapus Pengguna
+                  </Button>
+                </>
+              )}
+            />
+          </div>
+        );
+
+      default:
+        return cellValue;
+    }
+  }
+
+  if (error) {
+    return (
+      <Layout title="Custom Limitasi Pengguna">
+        <Container>
+          <ErrorPage
+            {...{
+              status_code: error.status_code,
+              message: error.error.message,
+              name: error.error.name,
+            }}
+          />
+        </Container>
+      </Layout>
+    );
+  }
+
+  if (isLoading) return <LoadingScreen />;
+
+  return (
+    <Layout title="Custom Limitasi Pengguna">
+      <Container className="gap-8">
+        <ButtonBack href="/ai" />
+
+        <TitleText
+          title="Custom Limitasi Pengguna 📋"
+          text="Semua data kustomisasi limit pengguna ada di sini."
+        />
+
+        <div className="grid">
+          <div className="sticky left-0 top-0 z-50 flex items-center justify-between gap-4 bg-white pb-4">
+            <SearchInput
+              placeholder="Cari Nama Pengguna atau ID Pengguna..."
+              defaultValue={query.q as string}
+              onChange={(e) => setSearch(e.target.value)}
+              onClear={() => setSearch("")}
+            />
+
+            <Button
+              color="secondary"
+              startContent={<Plus weight="bold" size={16} />}
+              className="w-max font-semibold"
+            >
+              Tambah Pengguna
+            </Button>
+          </div>
+
+          <div className="overflow-x-scroll scrollbar-hide">
+            <Table
+              isHeaderSticky
+              aria-label="limits user table"
+              color="secondary"
+              selectionMode="none"
+              classNames={customStyleTable}
+              className="scrollbar-hide"
+            >
+              <TableHeader columns={columnsLimitUser}>
+                {(column) => (
+                  <TableColumn key={column.uid}>{column.name}</TableColumn>
+                )}
+              </TableHeader>
+
+              <TableBody
+                items={data?.data.users}
+                emptyContent={<EmptyData text="Pengguna tidak ditemukan!" />}
+              >
+                {(user: LimitAIUser) => (
+                  <TableRow key={user.user_id}>
+                    {(columnKey) => (
+                      <TableCell>
+                        {renderCellLimitUsers(user, columnKey)}
+                      </TableCell>
+                    )}
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+
+        {data?.data.users.length ? (
+          <Pagination
+            isCompact
+            showControls
+            page={data?.data.page as number}
+            total={data?.data.total_pages as number}
+            onChange={(e) => {
+              router.push({
+                query: {
+                  ...router.query,
+                  page: e,
+                },
+              });
+            }}
+            className="justify-self-center"
+            classNames={{
+              cursor: "bg-purple text-white",
+            }}
+          />
+        ) : null}
+      </Container>
+    </Layout>
+  );
+}
+
+export const getServerSideProps = withToken(async (ctx) => {
+  const { query } = ctx;
+
+  return {
+    props: {
+      query: query as ParsedUrlQuery,
+    },
+  };
+});
--- types/ai/limit.type.ts
+++ types/ai/limit.type.ts
@@ -7,3 +7,23 @@ export interface LimitAI {
   created_by: string;
   updated_by: string;
 }
+
+// ===== seperated =====
+
+export type LimitAIUserResponse = {
+  users: LimitAIUser[];
+  page: number;
+  total_users: number;
+  total_pages: number;
+};
+
+export type LimitAIUser = {
+  user_id: string;
+  fullname: string;
+  total: number;
+  expired_at: string;
+  created_at: string;
+  updated_at: string;
+  created_by: string;
+  updated_by: string;
+};
