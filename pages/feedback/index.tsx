725e42cd0bb6a4b264954a9e947fcff51cc7209f
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-05-29 18:57:08 +0700

    chore(feedback): adjusment & modified layout feedback page

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -6,24 +6,18 @@ import TitleText from "@/components/TitleText";
 import Container from "@/components/wrapper/Container";
 import Layout from "@/components/wrapper/Layout";
 import useSearch from "@/hooks/useSearch";
+import { withToken } from "@/lib/getToken";
+import { getUrl } from "@/lib/getUrl";
 import { FeedbackResponse } from "@/types/feedback.type";
 import { SuccessResponse } from "@/types/global.type";
 import { formatDate } from "@/utils/formatDate";
 import { Avatar, Pagination } from "@nextui-org/react";
 import { Star } from "@phosphor-icons/react";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 import { useEffect } from "react";
 import useSWR from "swr";
-
-function getUrl(query: ParsedUrlQuery) {
-  if (query.q) {
-    return `/admin/feedback?q=${query.q}&page=${query.page ? query.page : 1}`;
-  }
-
-  return `/admin/feedback?page=${query.page ? query.page : 1}`;
-}
 
 export default function FeedbackPage({
   token,
@@ -32,7 +26,7 @@ export default function FeedbackPage({
   const router = useRouter();
   const { setSearch, searchValue } = useSearch(800);
   const { data, error, isLoading } = useSWR<SuccessResponse<FeedbackResponse>>({
-    url: getUrl(query) as string,
+    url: getUrl("/admin/feedback", query),
     method: "GET",
     token,
   });
@@ -84,114 +78,110 @@ export default function FeedbackPage({
 
   return (
     <Layout title="Feedback" className="scrollbar-hide">
-      <Container>
-        <section className="grid gap-8">
-          <TitleText
-            title="Masukan dan Saran ⭐"
-            text="Semua masukan dan saran akan tampil disini"
-          />
+      <Container className="gap-8">
+        <TitleText
+          title="Masukan dan Saran ⭐"
+          text="Semua masukan dan saran akan tampil disini"
+        />
 
-          <div className="grid">
-            <div className="sticky left-0 top-0 z-50 bg-white pb-4">
-              <SearchInput
-                placeholder="Cari User ID atau Nama User"
-                defaultValue={query.q as string}
-                onChange={(e) => setSearch(e.target.value)}
-                onClear={() => setSearch("")}
-              />
-            </div>
+        <div className="grid">
+          <div className="sticky left-0 top-0 z-50 bg-white pb-4">
+            <SearchInput
+              placeholder="Cari Nama Pengguna atau ID Pengguna..."
+              defaultValue={query.q as string}
+              onChange={(e) => setSearch(e.target.value)}
+              onClear={() => setSearch("")}
+            />
+          </div>
 
-            {searchValue && data?.data.feedback.length === 0 ? (
-              <EmptyData text="Feedback tidak ditemukan!" />
-            ) : (
-              <div className="grid grid-cols-2 items-start gap-x-4 gap-y-2">
-                {data?.data.feedback.map((data, index) => (
-                  <div
-                    key={index}
-                    className="grid gap-4 rounded-xl border-2 border-purple/10 p-6 hover:border-purple hover:bg-purple/10"
-                  >
-                    <div className="flex items-center justify-between gap-4">
-                      <div className="inline-flex items-center gap-4">
-                        <Avatar
-                          isBordered
-                          showFallback
-                          size="sm"
-                          src="/img/avatar.svg"
-                          classNames={{
-                            base: "ring-purple bg-purple/20",
-                            icon: "text-purple",
-                          }}
-                        />
+          {searchValue && data?.data.feedback.length === 0 ? (
+            <EmptyData text="Pengguna tidak ditemukan!" />
+          ) : (
+            <div className="grid grid-cols-2 items-start gap-x-4 gap-y-2">
+              {data?.data.feedback.map((data, index) => (
+                <div
+                  key={index}
+                  className="grid gap-4 rounded-xl border-2 border-purple/10 p-6 hover:border-purple hover:bg-purple/10"
+                >
+                  <div className="flex items-center justify-between gap-4">
+                    <div className="inline-flex items-center gap-4">
+                      <Avatar
+                        isBordered
+                        showFallback
+                        size="sm"
+                        src="/img/avatar.svg"
+                        classNames={{
+                          base: "ring-purple bg-purple/20",
+                          icon: "text-purple",
+                        }}
+                      />
 
-                        <div>
-                          <h3 className="max-w-[300px] text-sm font-bold text-black">
-                            {data.fullname}
-                          </h3>
-                          <p className="text-[12px] font-medium text-gray">
-                            {data.user_id}
-                          </p>
-                        </div>
-                      </div>
-
-                      <div className="grid gap-1">
-                        <p className="text-[12px] font-medium text-gray">
-                          Dibuat Pada:
-                        </p>
-                        <h3 className="text-sm font-semibold text-black">
-                          {formatDate(data.created_at)}
+                      <div>
+                        <h3 className="max-w-[300px] text-sm font-bold text-black">
+                          {data.fullname}
                         </h3>
+                        <p className="text-[12px] font-medium text-gray">
+                          {data.user_id}
+                        </p>
                       </div>
                     </div>
 
                     <div className="grid gap-1">
-                      <div className="inline-flex items-center gap-1">
-                        {renderStars(data.rating)}
-                      </div>
-
-                      <p className="text-sm font-medium leading-[170%] text-black">
-                        {data.text}
+                      <p className="text-[12px] font-medium text-gray">
+                        Dibuat Pada:
                       </p>
+                      <h3 className="text-sm font-semibold text-black">
+                        {formatDate(data.created_at)}
+                      </h3>
                     </div>
                   </div>
-                ))}
-              </div>
-            )}
-          </div>
 
-          {data?.data.feedback.length ? (
-            <Pagination
-              isCompact
-              showControls
-              page={data?.data.page as number}
-              total={data?.data.total_pages as number}
-              onChange={(e) => {
-                router.push({
-                  query: {
-                    ...router.query,
-                    page: e,
-                  },
-                });
-              }}
-              className="justify-self-center"
-              classNames={{
-                cursor: "bg-purple text-white",
-              }}
-            />
-          ) : null}
-        </section>
+                  <div className="grid gap-1">
+                    <div className="inline-flex items-center gap-1">
+                      {renderStars(data.rating)}
+                    </div>
+
+                    <p className="text-sm font-medium leading-[170%] text-black">
+                      {data.text}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+
+        {data?.data.feedback.length ? (
+          <Pagination
+            isCompact
+            showControls
+            page={data?.data.page as number}
+            total={data?.data.total_pages as number}
+            onChange={(e) => {
+              router.push({
+                query: {
+                  ...router.query,
+                  page: e,
+                },
+              });
+            }}
+            className="justify-self-center"
+            classNames={{
+              cursor: "bg-purple text-white",
+            }}
+          />
+        ) : null}
       </Container>
     </Layout>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{
-  token: string;
-  query: ParsedUrlQuery;
-}> = async ({ req, query }) => {
+export const getServerSideProps = withToken(async (ctx) => {
+  const { query } = ctx;
+
   return {
     props: {
-      token: req.headers["access_token"] as string,
-      query,
+      query: query as ParsedUrlQuery,
     },
   };
-};
+});
