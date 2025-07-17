db9a94d0a0a7f8f15059f32856269be8cc8e17b9
gufronnakaaw <gufronnakaaw@gmail.com>
2025-07-17 21:13:00 +0700

    feat(dashboard): add skeleton loading

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,4 @@
 import ErrorPage from "@/components/ErrorPage";
-import LoadingScreen from "@/components/loading/LoadingScreen";
 import TitleText from "@/components/TitleText";
 import Container from "@/components/wrapper/Container";
 import Layout from "@/components/wrapper/Layout";
@@ -8,7 +7,7 @@ import { DashboardResponse } from "@/types/dashboard.type";
 import { SuccessResponse } from "@/types/global.type";
 import { formatDayWithoutTime } from "@/utils/formatDate";
 import { formatRupiah } from "@/utils/formatRupiah";
-import { Button } from "@nextui-org/react";
+import { Button, Skeleton } from "@nextui-org/react";
 import {
   ArrowRight,
   Certificate,
@@ -120,8 +119,6 @@ export default function DashboardPage({
     );
   }
 
-  if (isLoading) return <LoadingScreen />;
-
   return (
     <Layout title="Dashboard" className="scrollbar-hide">
       <Container className="gap-12">
@@ -143,56 +140,61 @@ export default function DashboardPage({
           </div>
 
           <div className="grid grid-cols-2 items-start gap-4">
-            {dataDashboard.map((item, index) => (
-              <div
-                key={index}
-                className="relative isolate flex items-start gap-4 overflow-hidden rounded-xl border-2 border-purple/10 p-6 hover:border-purple hover:bg-purple/10"
-              >
-                <IconContext.Provider
-                  value={{
-                    weight: "duotone",
-                    size: 64,
-                    className: "text-purple",
-                  }}
-                >
-                  <item.icon />
-
-                  <div className="grid flex-1 gap-4">
-                    <div className="grid">
-                      <p className="text-sm font-medium text-gray">
-                        {item.title}
-                      </p>
-
-                      <h1 className="text-4xl font-extrabold text-black">
-                        {item.total}
-                      </h1>
-                    </div>
-
-                    <Button
-                      size="sm"
-                      color="secondary"
-                      endContent={
-                        <ArrowRight
-                          weight="bold"
-                          size={16}
-                          className="text-white"
-                        />
-                      }
-                      onClick={() => router.push(item.path as string)}
-                      className="w-max px-8 font-semibold"
+            {isLoading
+              ? Array.from({ length: dataDashboard.length }).map((_, index) => (
+                  <Skeleton className="h-40 w-full rounded-xl" key={index} />
+                ))
+              : dataDashboard.length &&
+                dataDashboard.map((item, index) => (
+                  <div
+                    key={index}
+                    className="relative isolate flex items-start gap-4 overflow-hidden rounded-xl border-2 border-purple/10 p-6 hover:border-purple hover:bg-purple/10"
+                  >
+                    <IconContext.Provider
+                      value={{
+                        weight: "duotone",
+                        size: 64,
+                        className: "text-purple",
+                      }}
                     >
-                      Selengkapnya
-                    </Button>
-                  </div>
-                </IconContext.Provider>
+                      <item.icon />
 
-                <item.icon
-                  weight="duotone"
-                  size={180}
-                  className="absolute -bottom-12 -right-12 -rotate-[25deg] text-purple/15"
-                />
-              </div>
-            ))}
+                      <div className="grid flex-1 gap-4">
+                        <div className="grid">
+                          <p className="text-sm font-medium text-gray">
+                            {item.title}
+                          </p>
+
+                          <h1 className="text-4xl font-extrabold text-black">
+                            {item.total}
+                          </h1>
+                        </div>
+
+                        <Button
+                          size="sm"
+                          color="secondary"
+                          endContent={
+                            <ArrowRight
+                              weight="bold"
+                              size={16}
+                              className="text-white"
+                            />
+                          }
+                          onClick={() => router.push(item.path as string)}
+                          className="w-max px-8 font-semibold"
+                        >
+                          Selengkapnya
+                        </Button>
+                      </div>
+                    </IconContext.Provider>
+
+                    <item.icon
+                      weight="duotone"
+                      size={180}
+                      className="absolute -bottom-12 -right-12 -rotate-[25deg] text-purple/15"
+                    />
+                  </div>
+                ))}
           </div>
         </div>
       </Container>
