d2aad7f0b31a58db3837f2cabbf696dc86ac21fe
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-07-27 15:27:20 +0700

    chore: add skeleton loading tryout univ page

--- pages/apotekerclass/tryout/[id]/index.tsx
+++ pages/apotekerclass/tryout/[id]/index.tsx
@@ -1,5 +1,6 @@
 import ButtonBack from "@/components/button/ButtonBack";
 import ErrorPage from "@/components/ErrorPage";
+import LoadingTitleImage from "@/components/loading/LoadingTitleImage";
 import VideoComponent from "@/components/VideoComponent";
 import Container from "@/components/wrapper/Container";
 import Layout from "@/components/wrapper/Layout";
@@ -12,6 +13,7 @@ import {
   AccordionItem,
   Button,
   Pagination,
+  Skeleton,
 } from "@nextui-org/react";
 import {
   CheckCircle,
@@ -66,35 +68,39 @@ export default function DetailsTryoutPage({
       <Container className="gap-8">
         <ButtonBack />
 
-        <div className="grid grid-cols-[max-content_1fr_max-content] items-end gap-4">
-          <ClipboardText
-            weight="duotone"
-            size={18}
-            className="size-full text-purple"
-          />
+        {isLoading ? (
+          <LoadingTitleImage />
+        ) : (
+          <div className="grid grid-cols-[max-content_1fr_max-content] items-end gap-4">
+            <ClipboardText
+              weight="duotone"
+              size={18}
+              className="size-full text-purple"
+            />
 
-          <div className="grid gap-1">
-            <h4 className="text-2xl font-bold capitalize -tracking-wide text-black">
-              {data?.data.title}
-            </h4>
+            <div className="grid gap-1">
+              <h4 className="text-2xl font-bold capitalize -tracking-wide text-black">
+                {data?.data.title}
+              </h4>
 
-            <p className="max-w-[800px] font-medium leading-[170%] text-gray">
-              {data?.data.description}
-            </p>
+              <p className="max-w-[800px] font-medium leading-[170%] text-gray">
+                {data?.data.description}
+              </p>
+            </div>
+
+            <Button
+              variant="light"
+              color="secondary"
+              startContent={<PencilLine weight="duotone" size={18} />}
+              onClick={() =>
+                router.push(`/apotekerclass/tryout/${data?.data.ass_id}/edit`)
+              }
+              className="font-semibold"
+            >
+              Edit Tryout
+            </Button>
           </div>
-
-          <Button
-            variant="light"
-            color="secondary"
-            startContent={<PencilLine weight="duotone" size={18} />}
-            onClick={() =>
-              router.push(`/apotekerclass/tryout/${data?.data.ass_id}/edit`)
-            }
-            className="font-semibold"
-          >
-            Edit Tryout
-          </Button>
-        </div>
+        )}
 
         <div className="grid">
           <h5 className="sticky left-0 top-0 z-50 bg-white pb-4 text-xl font-bold text-black">
@@ -102,80 +108,91 @@ export default function DetailsTryoutPage({
           </h5>
 
           <div className="grid gap-2 overflow-y-scroll scrollbar-hide">
-            {data?.data.questions.map((question, index) => (
-              <div
-                key={question.assq_id}
-                className="flex items-start gap-6 rounded-xl border-2 border-gray/20 p-6"
-              >
-                <div className="font-extrabold text-purple">{index + 1}.</div>
+            {isLoading
+              ? Array.from({ length: data?.data.questions.length || 2 }).map(
+                  (_, index) => (
+                    <Skeleton key={index} className="h-52 w-full rounded-xl" />
+                  ),
+                )
+              : data?.data.questions.map((question, index) => (
+                  <div
+                    key={question.assq_id}
+                    className="flex items-start gap-6 rounded-xl border-2 border-gray/20 p-6"
+                  >
+                    <div className="font-extrabold text-purple">
+                      {index + 1}.
+                    </div>
 
-                <div className="grid flex-1 gap-4">
-                  {question.type == "video" ? (
-                    <Suspense fallback={<p>Loading video...</p>}>
-                      <VideoComponent url={question.text as string} />
-                    </Suspense>
-                  ) : (
-                    <p
-                      className="preventive-list preventive-table list-outside text-[16px] font-semibold leading-[170%] text-black"
-                      dangerouslySetInnerHTML={{
-                        __html: question.text as string,
-                      }}
-                    />
-                  )}
+                    <div className="grid flex-1 gap-4">
+                      {question.type == "video" ? (
+                        <Suspense fallback={<p>Loading video...</p>}>
+                          <VideoComponent url={question.text as string} />
+                        </Suspense>
+                      ) : (
+                        <p
+                          className="preventive-list preventive-table list-outside text-[16px] font-semibold leading-[170%] text-black"
+                          dangerouslySetInnerHTML={{
+                            __html: question.text as string,
+                          }}
+                        />
+                      )}
 
-                  <div className="grid gap-1">
-                    {question.options.map((item, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className="inline-flex items-center gap-2"
+                      <div className="grid gap-1">
+                        {question.options.map((item, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className="inline-flex items-center gap-2"
+                            >
+                              {item.is_correct ? (
+                                <CheckCircle
+                                  weight="duotone"
+                                  size={18}
+                                  className="text-success"
+                                />
+                              ) : (
+                                <XCircle
+                                  weight="duotone"
+                                  size={18}
+                                  className="text-danger"
+                                />
+                              )}
+                              <p
+                                className={`flex-1 font-semibold ${
+                                  item.is_correct
+                                    ? "text-success"
+                                    : "text-gray/80"
+                                }`}
+                              >
+                                {item.text}
+                              </p>
+                            </div>
+                          );
+                        })}
+                      </div>
+
+                      <Accordion isCompact variant="bordered">
+                        <AccordionItem
+                          aria-label="accordion answer"
+                          key="answer"
+                          title="Penjelasan:"
+                          classNames={{
+                            title: "font-semibold text-black",
+                            content:
+                              "font-medium text-gray leading-[170%] pb-4",
+                          }}
                         >
-                          {item.is_correct ? (
-                            <CheckCircle
-                              weight="duotone"
-                              size={18}
-                              className="text-success"
-                            />
-                          ) : (
-                            <XCircle
-                              weight="duotone"
-                              size={18}
-                              className="text-danger"
-                            />
-                          )}
-                          <p
-                            className={`flex-1 font-semibold ${
-                              item.is_correct ? "text-success" : "text-gray/80"
-                            }`}
-                          >
-                            {item.text}
-                          </p>
-                        </div>
-                      );
-                    })}
+                          <div
+                            className="preventive-list preventive-table list-outside text-[16px] leading-[170%] text-black"
+                            dangerouslySetInnerHTML={{
+                              __html: question.explanation as string,
+                            }}
+                          ></div>
+                        </AccordionItem>
+                      </Accordion>
+                    </div>
                   </div>
-
-                  <Accordion isCompact variant="bordered">
-                    <AccordionItem
-                      aria-label="accordion answer"
-                      key="answer"
-                      title="Penjelasan:"
-                      classNames={{
-                        title: "font-semibold text-black",
-                        content: "font-medium text-gray leading-[170%] pb-4",
-                      }}
-                    >
-                      <div
-                        className="preventive-list preventive-table list-outside text-[16px] leading-[170%] text-black"
-                        dangerouslySetInnerHTML={{
-                          __html: question.explanation as string,
-                        }}
-                      ></div>
-                    </AccordionItem>
-                  </Accordion>
-                </div>
-              </div>
-            ))}
+                ))}
           </div>
         </div>
 
