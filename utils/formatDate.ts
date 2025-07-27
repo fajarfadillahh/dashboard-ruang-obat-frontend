6ae593eee0cd950fb41a0ddf676b7aa4bcd4f74a
gufronnakaaw <gufronnakaaw@gmail.com>
2025-07-27 21:04:15 +0700

    feat: add formatDateSimple

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -43,3 +43,17 @@ export function formatDayWithoutTime(date: Date) {
 
   return `${days[date.getDay()]}, ${day} ${months[month]} ${year}`;
 }
+
+export function formatDateSimple(dateProps: string, withTime = false): string {
+  const date = new Date(dateProps);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return withTime
+    ? `${day}/${month}/${year} ${hours}:${minutes}`
+    : `${day}/${month}/${year}`;
+}
