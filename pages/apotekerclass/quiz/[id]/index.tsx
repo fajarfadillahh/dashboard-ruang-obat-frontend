386b07468fef8f4deaca6a24904bad785ccf910e
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-07-27 21:55:04 +0700

    chore(pages): fix path for edit on card quiz apotekerclass

--- pages/apotekerclass/quiz/[id]/index.tsx
+++ pages/apotekerclass/quiz/[id]/index.tsx
@@ -142,7 +142,7 @@ export default function DetailCategoryQuizPage({
                     size="sm"
                     color="secondary"
                     onClick={() =>
-                      router.push(`/apotekerclass/quiz/${router.query.id}/edit`)
+                      router.push(`/apotekerclass/quiz/${quizz.ass_id}/edit`)
                     }
                     className="absolute right-4 top-4 z-50 hidden group-hover:flex"
                   >
