6f8e6e7fe3890df57d3335588ec25a98dd256af2
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-07-30 18:50:28 +0700

    style(pages): add property isStriped on table subscriptions

--- pages/videocourse/subscriptions/index.tsx
+++ pages/videocourse/subscriptions/index.tsx
@@ -244,6 +244,7 @@ export default function SubscriptionsPage({
 
           <div className="overflow-x-scroll scrollbar-hide">
             <Table
+              isStriped
               isHeaderSticky
               aria-label="subscriptions table"
               color="secondary"
