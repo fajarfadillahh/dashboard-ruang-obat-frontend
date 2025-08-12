991ca115b7fe312453f36b93abd423d86e7ce61e
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-12 13:14:33 +0700

    style(pages): adjust layout on testimonials page

--- pages/testimonials/index.tsx
+++ pages/testimonials/index.tsx
@@ -6,50 +6,51 @@ import { useRouter } from "next/router";
 
 export default function ProductsTestimonialPage() {
   const router = useRouter();
+
   return (
     <Layout title="Testimoni Produk - Ruang Obat" className="scrollbar-hide">
       <Container className="gap-8">
         <TitleText
           title="Testimoni Produk - Ruang Obat 📝"
-          text="Testimoni Produk yang tersedia pada Ruang Obat"
+          text="Testimoni produk yang tersedia pada Ruang Obat"
         />
 
-        <div className="grid">
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-            {products.map((product) => {
-              const Icon = product.icon;
-              return (
+        <div className="grid grid-cols-3 gap-4">
+          {products.map((product) => {
+            const Icon = product.icon;
+
+            return (
+              <div
+                key={product.code}
+                className="grid justify-items-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 p-8 text-sm hover:cursor-pointer hover:bg-purple/10"
+                onClick={() => router.push(`/testimonials/${product.code}`)}
+              >
                 <div
-                  key={product.code}
-                  className="group grid justify-items-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 p-8 text-sm hover:cursor-pointer hover:bg-purple/10"
-                  onClick={() => router.push(`/testimonials/${product.code}`)}
+                  className="flex size-16 items-center justify-center rounded-full"
+                  style={{
+                    backgroundColor:
+                      product.iconColor === "warning"
+                        ? "#FBBF24"
+                        : product.iconColor === "secondary"
+                          ? "#A78BFA"
+                          : product.iconColor === "success"
+                            ? "#34D399"
+                            : product.iconColor === "primary"
+                              ? "#6366F1"
+                              : product.iconColor === "danger"
+                                ? "#F87171"
+                                : "#E5E7EB",
+                  }}
                 >
-                  <div
-                    className="mb-2 flex h-16 w-16 items-center justify-center rounded-full"
-                    style={{
-                      backgroundColor:
-                        product.iconColor === "warning"
-                          ? "#FBBF24"
-                          : product.iconColor === "secondary"
-                            ? "#A78BFA"
-                            : product.iconColor === "success"
-                              ? "#34D399"
-                              : product.iconColor === "primary"
-                                ? "#6366F1"
-                                : product.iconColor === "danger"
-                                  ? "#F87171"
-                                  : "#E5E7EB",
-                    }}
-                  >
-                    <Icon size={36} color="#fff" weight="duotone" />
-                  </div>
-                  <h4 className="line-clamp-2 text-center font-extrabold text-black group-hover:line-clamp-none">
-                    {product.label}
-                  </h4>
+                  <Icon size={36} weight="duotone" className="text-white" />
                 </div>
-              );
-            })}
-          </div>
+
+                <h4 className="text-center font-extrabold text-black">
+                  {product.label}
+                </h4>
+              </div>
+            );
+          })}
         </div>
       </Container>
     </Layout>
