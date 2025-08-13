98ec5cd4bf444ff9b3d6f2a974b545821853e2ef
gufronnakaaw <gufronnakaaw@gmail.com>
2025-08-13 07:02:18 +0700

    feat(testimonials): implement files upload

--- pages/testimonials/[type].tsx
+++ pages/testimonials/[type].tsx
@@ -1,9 +1,13 @@
 import ButtonBack from "@/components/button/ButtonBack";
+import EmptyData from "@/components/EmptyData";
 import TitleText from "@/components/TitleText";
 import Container from "@/components/wrapper/Container";
 import Layout from "@/components/wrapper/Layout";
 import { withToken } from "@/lib/getToken";
 import { products } from "@/lib/products";
+import { SuccessResponse } from "@/types/global.type";
+import { TestimonialResponse } from "@/types/testimonial.type";
+import { fetcher } from "@/utils/fetcher";
 import {
   Button,
   Modal,
@@ -11,7 +15,9 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
+  Pagination,
   Skeleton,
+  Spinner,
   useDisclosure,
 } from "@nextui-org/react";
 import { ImageSquare, Plus, Trash } from "@phosphor-icons/react";
@@ -19,8 +25,10 @@ import { InferGetServerSidePropsType } from "next";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useQueryState } from "nuqs";
 import { ParsedUrlQuery } from "querystring";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
 import useSWR from "swr";
 
 export default function TestimonialListPage({
@@ -30,28 +38,105 @@ export default function TestimonialListPage({
   const router = useRouter();
   const session = useSession();
   const { onOpen, onOpenChange, isOpen, onClose } = useDisclosure();
-  const { data, isLoading, mutate } = useSWR({
-    url: `/testimonials?type=${params.type}`,
+  const [page, setPage] = useQueryState("page", { defaultValue: "1" });
+
+  const { data, isLoading, mutate } = useSWR<
+    SuccessResponse<TestimonialResponse>
+  >({
+    url: `/testimonials?type=${params.type}&page=${page}`,
     method: "GET",
     token,
   });
-  const [file, setFile] = useState<File | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   const product = products.find((item) => item.code === router.query.type);
 
-  async function handleAddTestimonial() {
-    const payload = {
-      image: file,
-      type: router.query.type,
-      by: session.data?.user.fullname,
-    };
+  async function handleAddTestimonial(local_files: File[]) {
+    setLoading(true);
+    try {
+      const response: SuccessResponse<{ key: string; url: string }[]> =
+        await fetcher({
+          url: "/storage/presigned",
+          method: "POST",
+          data: {
+            files: local_files.map((file) => ({
+              filename: file.name,
+              type: file.type,
+            })),
+            folder: "testimonials/",
+            by: session.data?.user.fullname,
+          },
+          token,
+        });
 
-    console.log(payload);
+      const newRequests: XMLHttpRequest[] = [];
+
+      local_files.forEach((file, i) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("PUT", response.data[i].url);
+        xhr.setRequestHeader("Content-Type", file.type);
+
+        xhr.onload = () => {
+          if (
+            local_files.every(
+              (_, idx) =>
+                newRequests[idx].readyState === 4 &&
+                newRequests[idx].status === 200,
+            )
+          ) {
+            fetcher({
+              url: "/batches/testimonials",
+              method: "POST",
+              data: {
+                keys: response.data.map((item) => item.key),
+                type: params.type,
+              },
+              token,
+            })
+              .then(() => {
+                toast.success("Berhasil mengunggah testimonial");
+                setLoading(false);
+                mutate();
+                onClose();
+              })
+              .catch(() => {
+                toast.error("Gagal mengunggah file, silakan coba lagi.");
+                setLoading(false);
+              });
+          }
+        };
+
+        xhr.onerror = () => {
+          toast.error("Gagal upload file, silakan coba lagi.");
+          setLoading(false);
+        };
+
+        xhr.send(file);
+        newRequests.push(xhr);
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Gagal mengunggah file, silakan coba lagi.");
+    }
   }
 
   async function handleDeleteTestimonial(testimonial_id: string) {
-    alert(`ID: ${testimonial_id} successfully deleted`);
+    if (!confirm("apakah anda yakin?")) return;
+
+    try {
+      await fetcher({
+        url: `/testimonials/${testimonial_id}`,
+        method: "DELETE",
+        token,
+      });
+
+      toast.success("Berhasil menghapus testimonial");
+      mutate();
+    } catch (error) {
+      console.error(error);
+      toast.error("Gagal menghapus testimonial");
+    }
   }
 
   return (
@@ -75,7 +160,7 @@ export default function TestimonialListPage({
           />
         )}
 
-        <div className="grid">
+        <div className="grid" ref={divRef}>
           <div className="sticky left-0 top-0 z-50 flex items-center justify-end gap-4 bg-white pb-4">
             <Button
               color="secondary"
@@ -88,104 +173,126 @@ export default function TestimonialListPage({
 
             <Modal
               isDismissable={false}
+              hideCloseButton={loading}
               placement="center"
               scrollBehavior="inside"
               isOpen={isOpen}
               onOpenChange={onOpenChange}
               onClose={() => {
                 onClose();
-                setFile(null);
               }}
             >
               <ModalContent>
-                {(onClose) => (
-                  <>
-                    <ModalHeader className="font-extrabold text-black">
-                      Tambah Testimonial
-                    </ModalHeader>
+                <ModalHeader className="font-extrabold text-black">
+                  Tambah Testimonial
+                </ModalHeader>
 
-                    <ModalBody>
-                      <div className="grid w-full gap-4 rounded-xl border border-dashed border-gray/30 bg-gray/5 py-8">
-                        <ImageSquare
-                          weight="duotone"
-                          size={42}
-                          className="justify-self-center text-purple"
-                        />
+                <ModalBody>
+                  <div
+                    className="grid w-full gap-4 rounded-xl border border-dashed border-gray/30 bg-gray/5 py-8"
+                    onDragOver={(e) => {
+                      e.preventDefault();
+                    }}
+                    onDragLeave={(e) => {
+                      e.preventDefault();
+                    }}
+                    onDrop={(e) => {
+                      e.preventDefault();
+                      handleAddTestimonial(Array.from(e.dataTransfer.files));
+                    }}
+                  >
+                    <ImageSquare
+                      weight="duotone"
+                      size={42}
+                      className="justify-self-center text-purple"
+                    />
 
-                        <label className="flex items-center justify-center">
+                    <label className="flex items-center justify-center">
+                      {loading ? (
+                        <Spinner color="secondary" />
+                      ) : (
+                        <>
                           <input
+                            multiple
                             hidden
                             accept=".jpg, .jpeg, .png"
                             type="file"
                             onChange={(e) => {
                               if (!e.target.files?.length) return;
-                              setFile(e.target.files[0]);
+                              handleAddTestimonial(Array.from(e.target.files));
                             }}
                           />
 
                           <div className="flex h-9 w-28 cursor-pointer flex-col items-center justify-center rounded-full bg-purple px-2 text-xs font-semibold leading-4 text-white shadow focus:outline-none">
-                            {file ? file.name : "Import Data"}
+                            klik atau drag
                           </div>
-                        </label>
-                      </div>
-                    </ModalBody>
+                        </>
+                      )}
+                    </label>
+                  </div>
+                </ModalBody>
 
-                    <ModalFooter>
-                      <div className="inline-flex items-center gap-2">
-                        <Button
-                          color="danger"
-                          variant="light"
-                          onPress={() => {
-                            onClose();
-                            setFile(null);
-                          }}
-                          className="px-6 font-bold"
-                        >
-                          Tutup
-                        </Button>
-
-                        <Button
-                          color="secondary"
-                          onClick={handleAddTestimonial}
-                          className="px-6 font-semibold"
-                        >
-                          Simpan Testimonial
-                        </Button>
-                      </div>
-                    </ModalFooter>
-                  </>
-                )}
+                <ModalFooter></ModalFooter>
               </ModalContent>
             </Modal>
           </div>
 
           <div className="grid grid-cols-5 gap-4">
-            {Array.from({ length: 4 }).map((_, index) => (
-              <div
-                key={index}
-                className="group relative isolate grid h-auto items-center justify-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 p-8 text-sm hover:cursor-pointer hover:bg-purple/10"
-              >
-                <Image
-                  src="https://ruangobat.is3.cloudhost.id/statics/images/ruangobat-logo/default-thumbnail.png"
-                  alt="testimonial image"
-                  width={500}
-                  height={500}
-                  className="size-full rounded-lg object-cover object-center"
-                  priority
-                />
-
-                <Button
-                  isIconOnly
-                  color="danger"
-                  size="sm"
-                  onClick={() => handleDeleteTestimonial(index.toString())}
-                  className="absolute right-4 top-4 z-40 hidden group-hover:flex"
+            {isLoading ? (
+              Array.from({ length: data?.data.testimonials.length || 10 }).map(
+                (_, index) => (
+                  <Skeleton key={index} className="h-40 w-full rounded-xl" />
+                ),
+              )
+            ) : data?.data.testimonials.length ? (
+              data?.data.testimonials.map((item) => (
+                <div
+                  key={item.testimonial_id}
+                  className="group relative isolate grid h-auto items-center justify-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 p-8 text-sm hover:cursor-pointer hover:bg-purple/10"
+                  onClick={() => window.open(item.img_url, "_blank")}
                 >
-                  <Trash weight="duotone" size={18} />
-                </Button>
+                  <Image
+                    src={item.img_url}
+                    alt="testimonial image"
+                    width={500}
+                    height={500}
+                    className="size-full rounded-lg object-cover object-center"
+                    priority
+                  />
+
+                  <Button
+                    isIconOnly
+                    color="danger"
+                    size="sm"
+                    onClick={() => handleDeleteTestimonial(item.testimonial_id)}
+                    className="absolute right-4 top-4 z-40 hidden group-hover:flex"
+                  >
+                    <Trash weight="duotone" size={18} />
+                  </Button>
+                </div>
+              ))
+            ) : (
+              <div className="col-span-5 flex items-center justify-center rounded-xl border-2 border-dashed border-gray/20">
+                <EmptyData text="Testimoni Belum Tersedia!" />
               </div>
-            ))}
+            )}
           </div>
+          {!isLoading && data?.data.testimonials.length ? (
+            <Pagination
+              isCompact
+              showControls
+              page={data?.data.page as number}
+              total={data?.data.total_pages as number}
+              onChange={(e) => {
+                divRef.current?.scrollIntoView({ behavior: "smooth" });
+                setPage(`${e}`);
+              }}
+              className="mt-5 justify-self-center"
+              classNames={{
+                cursor: "bg-purple text-white",
+              }}
+            />
+          ) : null}
         </div>
       </Container>
     </Layout>
